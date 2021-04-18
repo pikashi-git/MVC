1d5b37b715a3bbdea5bfd93b6ce35b3feec618b4
pikashi-git <80340904+pikashi-git@users.noreply.github.com>
2021-04-18 12:51:25 +0800

    202104181251-home-noteook

--- MVC網站/Controllers/Test1Controller.cs
+++ MVC網站/Controllers/Test1Controller.cs
@@ -1,19 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MVC網站.ViewModels.Test1;
+using System;
 using System.IO;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MVC網站.Controllers
 {
     public class Test1Controller : Controller
     {
-        public ActionResult Index(string name)
+        public ActionResult Index(string name, int? age, string phone, DateTime? time, FormCollection form, [Bind(Exclude = "phone,time")] Test1Model testNodel)
         {
             ViewBag.Title = "Test Home Page";
             ViewBag.Page = "Test!";
-            ViewData["name"] = name;
+
+            //ViewData["name"] = name;
+            //ViewData["age"] = age;
+            //ViewData["phone"] = phone;
+            //ViewData["time"] = time;
+
+            //ViewData["name"] = form["name"];
+            //ViewData["age"] = form["age"];
+            //ViewData["phone"] = form["phone"];
+            //ViewData["time"] = form["time"];
+
+            if (ModelState.IsValid)
+            {
+                ViewData["name"] = testNodel.name;
+                ViewData["age"] = testNodel.age != null ? testNodel.age.ToString() : "";
+                ViewData["phone"] = testNodel.phone;
+                ViewData["time"] = testNodel.time;
+
+                ViewBag.Message = "資料通過驗證";
+            }
+            else
+            {
+                ModelState.Clear();
+                ViewBag.Message = "資料未通過驗證";
+            }
             return View();
         }
         public ContentResult ForContentResult()
---
+++ MVC網站/ViewModels/Test1/Test1Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC網站.ViewModels.Test1
+{
+    public class Test1Model
+    {
+        [Required]
+        public string name { get; set; }
+        public int? age { get; set; }
+        public string phone { get; set; }
+        public DateTime? time { get; set; }
+    }
+}
