c5eb2d2bb51ec2c8068e2a0220226654ab67df0b
pikashi-git <80340904+pikashi-git@users.noreply.github.com>
2021-06-02 08:51:48 +0800

    202106020851-desktop

---
+++ 留言板實作/Models/guestbook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace 留言板實作.Models
+{
+    public class guestbook
+    {
+        public int ID { get; set; }
+        public int userID { get; set; }
+        public string postContent { get; set; }
+        public int parent { get; set; }
+        public DateTime createtime { get; set; }
+    }
+}
---
+++ 留言板實作/Models/users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace 留言板實作.Models
+{
+    public class users
+    {
+        public int userID { get; set; }
+        public string names { get; set; }
+        public string nick { get; set; }
+    }
+}
